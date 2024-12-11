173febe85be143882ab46be67b9640237d25af30
Muhammad Akhtar <105409960+M-Dan-Akhtar@users.noreply.github.com>
2024-12-10 20:10:10 -0500

    Added background

--- Roche Limit/Assets/Scripts/Teleporter.cs
+++ Roche Limit/Assets/Scripts/Teleporter.cs
@@ -13,12 +13,19 @@ public class Teleporter : MonoBehaviour
     private Vector3 previousFacing;
     private Vector3 previousCamera;
 
+    private GameObject clouds;
+    private GameObject dust;
+    private bool toggleTimeLineClouds = false;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
         camera = GameObject.Find("Camera");
         alternateStart = GameObject.Find("AlternateTimelineStart").transform;
+        clouds = GameObject.Find("Clouds");
+        dust = GameObject.Find("Dust");
+        toggleClouds();
 
         previousLocation = new Vector3(alternateStart.position.x, alternateStart.position.y, alternateStart.position.z);
         previousCamera = new Vector3(alternateStart.position.x, camera.transform.position.y + 100.0f, camera.transform.position.z);
@@ -32,7 +39,7 @@ public class Teleporter : MonoBehaviour
 
         if (Input.GetKeyDown("c"))
         {
-
+          
             player.GetComponent<SpriteRenderer>().enabled = false;
             player.GetComponent<Animator>().enabled = false;
             player.GetComponent<PlayerMovement>().enabled = false;
@@ -42,12 +49,19 @@ public class Teleporter : MonoBehaviour
         
     }
 
+    private void toggleClouds(){
+      clouds.GetComponent<SpriteRenderer>().enabled = toggleTimeLineClouds;
+      dust.GetComponent<SpriteRenderer>().enabled = toggleTimeLineClouds;
+      toggleTimeLineClouds = !toggleTimeLineClouds;
+    }
+
     IEnumerator WaitAndTeleport(int s)
     {       
         Vector3 currentLocation = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
         Vector3 currentCamera = new Vector3(camera.transform.position.x, camera.transform.position.y, camera.transform.position.z);
         Vector3 currentFacing = new Vector3(player.transform.localScale.x, player.transform.localScale.y, player.transform.localScale.z);
 
+        toggleClouds();
         player.transform.position = previousLocation;
         player.transform.localScale = previousFacing;
         camera.transform.position = previousCamera;
